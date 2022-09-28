d36d56cfaf6cf38b0c8df977db80adfaa06219cf
Quinten Krauth <quintenkrauth19@gmail.com>
2022-09-28 17:02:59 -0400

    final

--- js-interactivity/index.js
+++ js-interactivity/index.js
@@ -1,3 +1,5 @@
+const message = document.querySelector("#message");
+
 console.log("Hello World");
 
 const addMovie = (event) => {
@@ -6,8 +8,24 @@ const addMovie = (event) => {
     let movie = document.createElement("li");
     let movieTitle = document.createElement("span");
     movieTitle.textContent = inputField.value
+    movieTitle.addEventListener("click", crossOffMovie)
     movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener("click", deleteMovie)
+    movie.appendChild(deleteBtn)
     document.querySelector("ul").appendChild(movie)
     inputField.value = ("")
 }
 document.querySelector("form").addEventListener("submit", (addMovie))
+
+const deleteMovie = (event) => {
+    event.target.parentNode.remove("li");
+    message.textContent = "Movie deleted"
+}
+
+const crossOffMovie = (event) => {
+    event.target.classList.toggle("checked");
+
+}
